9f89b1f34f50df8d7b7ceb7fafb71504cf3d4a95
Lawrence Kuria <i3uyer2@gmail.com>
2025-07-19 14:30:10 -0700

    Added filtering functionality

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const itemForm = document.getElementById('item-form')
 const itemInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list')
 const clearBtn = document.getElementById('clear')
+const filterIcon = document.getElementById('filter')
 
 function addItem (e) {
   e.preventDefault()
@@ -32,6 +33,8 @@ function addItem (e) {
   itemList.appendChild(listEle)
 
   itemInput.value = ''
+
+  checkUI()
 }
 
 
@@ -39,13 +42,46 @@ function removeItem (e) {
   if (e.target.tagName === 'I') {
     e.target.parentElement.parentElement.remove()
   }
+  checkUI()
 }
 
 
 function clearAll () {
-  itemList.innerHTML = ''
+  while (itemList.firstChild) {
+    itemList.firstChild.remove()
+  }
+
+  checkUI()
 }
+
+function checkUI () {
+  const items = document.querySelectorAll('li')
+  if (items.length === 0) {
+    clearBtn.style.display = 'none'
+    filterIcon.style.display = 'none'
+  } else {
+    clearBtn.style.display = 'block'
+    filterIcon.style.display = 'block'
+  }
+}
+
+function filterItems (e) {
+  const filter = e.target.value.toLowerCase()
+  const items = document.querySelectorAll('li')
+  items.forEach(item => 
+    {if (!item.firstChild.textContent.toLowerCase().includes(filter)) {
+      item.style.display = 'none'
+    } else {
+      item.style.display = 'flex'
+    }}
+    )
+}
+
 
 clearBtn.addEventListener('click', clearAll)
 itemList.addEventListener('click', removeItem)
 itemForm.addEventListener('submit', addItem)
+filterIcon.addEventListener('input', filterItems)
+
+
+checkUI()
